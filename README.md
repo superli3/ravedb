bb34e4d06163cf6edc705e9d5fe4f7d9efae46c8
superli3 <jeffli930@gmail.com>
2017-12-20 21:13:06 -0500

    Implement basic login

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #RaveDB code
 
-This is the code for the RaveDB application. WIP
+This is the code for the RaveDB application. Work in Progress
+
+Special thanks and credit to Michael Hartl's Ruby on Rails tutorial - https://www.railstutorial.org/
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-    		password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -58,7 +58,7 @@ class UserTest < ActiveSupport::TestCase
 
 
   test "email addresses should be saved as lower-case" do
-    mixed_case_email = "Foo@ExAMPle.CoM"
+    mixed_case_email = "JeFFLi1@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
