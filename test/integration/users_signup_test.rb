fc0d124cbf8f5d1348bae6bdae29bb18763382e1
superli3 <jeffli930@gmail.com>
2017-12-21 18:20:27 -0500

    Add Username Regex

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,7 +18,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { username:  "Example User",
+      post users_path, params: { user: { username:  "ExampleUser",
                                          email: "user@example.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
